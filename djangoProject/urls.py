7f7de96cfb4db8dff05d0b31945620081f31ddd6
Bhavin Gajjar <bhavin.gajjar@albanero.io>
2022-09-29 17:39:45 +0530

    rest_registration package added for registration things

--- djangoProject/urls.py
+++ djangoProject/urls.py
@@ -14,8 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+api_urlpatterns = [
+    path('accounts/', include('rest_registration.api.urls')),
+]
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # add apis urls
+    path('api/v1/', include(api_urlpatterns)),
 ]
